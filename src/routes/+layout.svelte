2561a65a5d7579a505a4c2adde769659443cdb97
ImNickDDMe <nickpapanastasis9@gmail.com>
2023-05-15 22:15:13 +0300

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.postcss';
+</script>
+
+<svelte:head>
+	<title>Nikolaos Papanastasis</title>
+</svelte:head>
+
+<main>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { Heading, Span, P } from "flowbite-svelte";
+</script>
+
+<section class="grid grid-rows-2 lg:p-64 p-24">
+    <div class="mb-12">
+        <Heading class="text-center lg:text-7xl text-6xl" tag="h1">
+            <Span gradient>Coming</Span> Soon...
+        </Heading>
+    </div>
+    <div>
+        <P weight="light" class="text-center text-xl">&copy; 2023 Nikolaos Papanastasis</P>
+    </div>
+</section>
